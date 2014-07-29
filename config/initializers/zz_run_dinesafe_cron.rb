13dbc4f2d8c875b59d56513e40939f079ab16f6c
Elias Tabello <etabw3@gmail.com>
2014-07-28 20:34:56 -0400

    tweaked cron task

--- config/initializers/zz_run_dinesafe_cron.rb
+++ config/initializers/zz_run_dinesafe_cron.rb
@@ -1,5 +1,7 @@
-puts 'Starting Cron task. Will download the dinesafe archive every 6 hours'.colorize(:green)
+cron_length_in_hours = 12.to_s
+cron_job = cron_length_in_hours + 'h'
+puts "Starting Cron task. Will download the dinesafe archive every #{cron_length_in_hours} hours".colorize(:green)
 
-Rufus::Scheduler.new.every '6h' do
+Rufus::Scheduler.new.every cron_job do
   system('rake dinesafe:grab')
 end
